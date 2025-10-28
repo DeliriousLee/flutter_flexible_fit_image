364add8104c1592b877026e18d380b2309b29fd9
deliriousLee <uniquepilot@icloud.com>
2025-10-28 16:09:36 +0800

    edit：example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -2,36 +2,58 @@ import 'package:flexible_fit_image/flexible_fit_image.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  double width = 200;
-  double height = 300;
+  double width = 300;
+  double height = 200;
 
-  /// 默认的按等比 撑满容器
-  BoxFit _defaultRatioFillFit(Size originSize) {
-    double windowRatio = 1.0;
-    if (width != null && height != null && height != 0) {
-      windowRatio = width / height;
-    }
-    double imgOriginRatio = originSize.width / originSize.height;
-    // 图片比显示 扁
-    if (imgOriginRatio > windowRatio) {
-      return BoxFit.fitHeight;
-    } else {
-      // 图片比显示 窄
-      return BoxFit.fitWidth;
-    }
-  }
-
+  double aspectRatio = 16 / 9;
+  String imagUrl =
+      'http://gips2.baidu.com/it/u=4160611580,2154032802&fm=3028&app=3028&f=JPEG&fmt=auto?w=720&h=1280';
   runApp(MaterialApp(
     home: Scaffold(
       appBar: AppBar(title: Text('FlexibleFitImage Example')),
-      body: Center(
-        child: FlexibleFitImage(
-          imageUrl: 'https://www.example.com/image.jpg',
-          width: 300,
-          height: 200,
-          selectFit: _defaultRatioFillFit, // 使用 _defaultRatioFillFit 函数
-          borderRadius: 10,
-        ),
+      body: Column(
+        children: [
+          FlexibleFitImage(
+            imageUrl: imagUrl,
+            width: width,
+            height: height,
+            selectFit: (Size originSize) {
+              double windowRatio;
+
+              windowRatio = width / height;
+
+              double imgOriginRatio = originSize.width / originSize.height;
+              // 图片比显示 扁
+              if (imgOriginRatio > windowRatio) {
+                return BoxFit.fitHeight;
+              } else {
+                // 图片比显示 窄
+                return BoxFit.fitWidth;
+              }
+            }, // 使用 _defaultRatioFillFit 函数
+            borderRadius: 10,
+          ),
+          FlexibleFitImage(
+            imageUrl: imagUrl,
+            width: double.infinity,
+            aspectRatio: aspectRatio,
+            selectFit: (Size originSize) {
+              double windowRatio;
+
+              windowRatio = aspectRatio;
+
+              double imgOriginRatio = originSize.width / originSize.height;
+              // 图片比显示 扁
+              if (imgOriginRatio > windowRatio) {
+                return BoxFit.fitHeight;
+              } else {
+                // 图片比显示 窄
+                return BoxFit.fitWidth;
+              }
+            }, // 使用 _defaultRatioFillFit 函数
+            borderRadius: 10,
+          )
+        ],
       ),
     ),
   ));
